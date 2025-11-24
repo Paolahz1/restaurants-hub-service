0468b1a338f7143e7ebc02f0e7a7313130645cf2
Paola <hpaolahz1@gmail.com>
2025-11-23 22:47:44 -0500

    añadido smás escenarios de test para crear un restaurante

--- src/test/java/com/foodcourt/hub/domain/usecase/restaurant/CreateRestaurantUseCaseTest.java
+++ src/test/java/com/foodcourt/hub/domain/usecase/restaurant/CreateRestaurantUseCaseTest.java
@@ -1,5 +1,9 @@
 package com.foodcourt.hub.domain.usecase.restaurant;
 
+import com.foodcourt.hub.domain.exception.DatabaseException;
+import com.foodcourt.hub.domain.exception.InvalidPhoneNumberException;
+import com.foodcourt.hub.domain.exception.InvalidRestaurantNameException;
+import com.foodcourt.hub.domain.exception.UserIsNotOwnerException;
 import com.foodcourt.hub.domain.model.Restaurant;
 import com.foodcourt.hub.domain.port.spi.IRestaurantPersistencePort;
 import com.foodcourt.hub.domain.port.spi.IUserVerificationPort;
@@ -8,10 +12,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.transaction.TransactionSystemException;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.argThat;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class CreateRestaurantUseCaseTest {
@@ -24,10 +30,8 @@ class CreateRestaurantUseCaseTest {
     @InjectMocks
     CreateRestaurantUseCase useCase;
 
-
-    @Test
-    void shouldCreateRestaurantSuccessfully() {
-        Restaurant testRestaurant = Restaurant.builder()
+    private Restaurant createValidRestaurant() {
+        return Restaurant.builder()
                 .name("La Buena Mesa")
                 .nit("123456789")
                 .address("Calle 123")
@@ -35,18 +39,63 @@ class CreateRestaurantUseCaseTest {
                 .urlLogo("http://logo.com/logo.png")
                 .ownerId(1L)
                 .build();
+    }
 
-        when(persistencePort.existsByNit(testRestaurant.getNit())).thenReturn(false);
-        when(verificationPort.getUserRole(testRestaurant.getOwnerId())).thenReturn("OWNER");
-        when(persistencePort.saveRestaurant(testRestaurant)).thenReturn(testRestaurant);
+    @Test
+    void shouldCreateRestaurantSuccessfully() {
+        Restaurant restaurant = createValidRestaurant();
 
-       useCase.create(testRestaurant);
+        when(persistencePort.existsByNit(restaurant.getNit())).thenReturn(false);
+        when(verificationPort.getUserRole(restaurant.getOwnerId())).thenReturn("OWNER");
+        when(persistencePort.saveRestaurant(restaurant)).thenReturn(restaurant);
 
-        verify(persistencePort).saveRestaurant(argThat(r ->
-                r.getName().equals("La Buena Mesa") &&
-                        r.getNit().equals("123456789") &&
-                        r.getOwnerId().equals(1L)
-        ));
+        useCase.create(restaurant);
+
+        verify(persistencePort).saveRestaurant(restaurant);
+    }
+
+    @Test
+    void shouldThrowUserIsNotOwnerException() {
+        Restaurant restaurant = createValidRestaurant();
+
+        when(verificationPort.getUserRole(restaurant.getOwnerId())).thenReturn("EMPLOYEE");
+
+        assertThrows(UserIsNotOwnerException.class, () -> useCase.create(restaurant));
+        verify(persistencePort, never()).saveRestaurant(any());
+    }
+
+
+    @Test
+    void shouldThrowInvalidPhoneNumberException() {
+        Restaurant restaurant = createValidRestaurant();
+        restaurant.setPhoneNumber("123ABC");
+
+        when(verificationPort.getUserRole(restaurant.getOwnerId())).thenReturn("OWNER");
+
+        assertThrows(InvalidPhoneNumberException.class, () -> useCase.create(restaurant));
+        verify(persistencePort, never()).saveRestaurant(any());
+    }
+
+    @Test
+    void shouldThrowDatabaseExceptionOnDataIntegrityViolation() {
+        Restaurant restaurant = createValidRestaurant();
+
+        when(verificationPort.getUserRole(restaurant.getOwnerId())).thenReturn("OWNER");
+        when(persistencePort.existsByNit(restaurant.getNit())).thenReturn(false);
+        when(persistencePort.saveRestaurant(restaurant)).thenThrow(DataIntegrityViolationException.class);
+
+        assertThrows(DatabaseException.class, () -> useCase.create(restaurant));
+    }
+
+    @Test
+    void shouldThrowDatabaseExceptionOnTransactionSystemException() {
+        Restaurant restaurant = createValidRestaurant();
+
+        when(verificationPort.getUserRole(restaurant.getOwnerId())).thenReturn("OWNER");
+        when(persistencePort.existsByNit(restaurant.getNit())).thenReturn(false);
+        when(persistencePort.saveRestaurant(restaurant)).thenThrow(TransactionSystemException.class);
+
+        assertThrows(DatabaseException.class, () -> useCase.create(restaurant));
     }
 
 }
