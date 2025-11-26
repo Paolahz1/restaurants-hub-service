8030428ad5929ab3cb520a452ec722323fd37cc1
Paola <hpaolahz1@gmail.com>
2025-11-26 02:35:00 -0500

    feat: teas para CreateDishUseCase

---
+++ src/test/java/com/foodcourt/hub/domain/usecase/CreateDishUseCaseTest.java
@@ -0,0 +1,58 @@
+package com.foodcourt.hub.domain.usecase;
+
+import com.foodcourt.hub.domain.exception.InvalidPermissionException;
+import com.foodcourt.hub.domain.model.Dish;
+import com.foodcourt.hub.domain.port.spi.IDishPersistencePort;
+import com.foodcourt.hub.domain.port.spi.IRestaurantPersistencePort;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CreateDishUseCaseTest {
+
+    @Mock
+    IDishPersistencePort dishPersistencePort;
+
+    @Mock
+    IRestaurantPersistencePort restaurantPersistencePort;
+
+    @InjectMocks
+    CreateDishUseCase useCase;
+
+
+    @Test
+    void shouldSaveDishWhenOwnerIsCorrect() {
+        // Arrange
+        Dish dish = new Dish();
+        dish.setRestaurantId(1L);
+        Long ownerId = 10L;
+
+        when(restaurantPersistencePort.getOwnerIdByRestaurant(1L)).thenReturn(ownerId);
+
+        // Act
+        useCase.create(dish, ownerId);
+
+        // Assert
+        verify(dishPersistencePort).saveDish(dish);
+    }
+
+    @Test
+    void shouldThrowInvalidPermissionExceptionWhenOwnerIsWrong() {
+        // Arrange
+        Dish dish = new Dish();
+        dish.setRestaurantId(1L);
+        Long ownerId = 10L;
+
+        when(restaurantPersistencePort.getOwnerIdByRestaurant(1L)).thenReturn(99L);
+
+        // Act & Assert
+        assertThrows(InvalidPermissionException.class, () -> useCase.create(dish, ownerId));
+    }
+}
